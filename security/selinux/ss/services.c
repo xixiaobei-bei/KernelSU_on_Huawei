66fffa7958d66e65f09466af18e28971b1d4750a
xixiaobei <110380709+xixiaobei-bei@users.noreply.github.com>
2026-03-15 21:13:25 +0800

    Backed ReSukiSU changes

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -73,7 +73,7 @@ int selinux_policycap_netpeer;
 int selinux_policycap_openperm;
 int selinux_policycap_alwaysnetwork;
 
-DEFINE_RWLOCK(policy_rwlock);
+static DEFINE_RWLOCK(policy_rwlock);
 
 static struct sidtab sidtab;
 struct policydb policydb;
