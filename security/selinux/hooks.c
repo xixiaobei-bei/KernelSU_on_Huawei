bc48e36163d737a5afd7f059ac224e8ea0d0bb28
Coconut <85353552+Coconutat@users.noreply.github.com>
2026-03-17 21:59:48 +0800

    ReSukiSU: Hooks.c: deprecated is_ksu_transition
    Sync change from:https://github.com/ReSukiSU/ReSukiSU/commit/b9f02702328fcee3f1a23d35965d5d63604f45d7

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -2287,11 +2287,6 @@ static u32 ptrace_parent_sid(struct task_struct *task)
 	return sid;
 }
 
-#ifdef CONFIG_KSU_MANUAL_HOOK
-extern bool is_ksu_transition(const struct task_security_struct *old_tsec, 
-				const struct task_security_struct *new_tsec);
-#endif
-
 static int check_nnp_nosuid(const struct linux_binprm *bprm,
 			    const struct task_security_struct *old_tsec,
 			    const struct task_security_struct *new_tsec)
@@ -2305,11 +2300,7 @@ static int check_nnp_nosuid(const struct linux_binprm *bprm,
 
 	if (new_tsec->sid == old_tsec->sid)
 		return 0; /* No change in credentials */
-
-   #ifdef CONFIG_KSU_MANUAL_HOOK
-	if (is_ksu_transition(old_tsec, new_tsec))
-		return 0;
-   #endif
+	
 	/*
 	 * The only transitions we permit under NNP or nosuid
 	 * are transitions to bounded SIDs, i.e. SIDs that are
