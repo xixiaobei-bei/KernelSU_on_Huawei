bf5efa3825798c08bf0e8a1134dc385c1b4be533
xixiao_bei <jiaxi120516@outlook.com>
2026-06-07 19:38:20 +0800

    Added RKSU support for MAR device on EMUI10.0.

---
+++ sound/soc/codecs/hisi/hi6555/hi6555_reg.h
@@ -0,0 +1,23 @@
+/*
+ * hi6555_ultr.h -- codec driver
+ *
+ * Copyright (c) 2019 Hisilicon Technologies CO., Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _HI6555_REG_H_
+#define _HI6555_REG_H_
+#ifdef CONFIG_SND_SOC_CODEC_HI6555V3
+#include "hi6555v3_asp_reg_def.h"
+#include "hi6555v3_pmu_reg_def.h"
+#endif
+
+#ifdef CONFIG_SND_SOC_CODEC_HI6555V5
+#include "hi6555v5_asp_reg_def.h"
+#include "hi6555v5_pmu_reg_def.h"
+#endif
+
+#endif
